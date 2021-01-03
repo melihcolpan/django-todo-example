5233e1a09a713627f23d88a4bee225d0dc40b36c
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-03 15:14:22 +0300

    - Debug mode closed.

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "<FAKE-SECRET>"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["3.9.39.158"]
 
