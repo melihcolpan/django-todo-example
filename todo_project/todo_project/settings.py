455d328db0a8ff24cc63cf13add4c6de028b34b9
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-03 09:54:25 +0300

    - Main urls edited. - settings.py edited.

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -44,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+SESSION_COOKIE_AGE = 120960000
--- todo_project/todo_project/urls.py
+++ todo_project/todo_project/urls.py
@@ -21,7 +21,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('todos/', include('todo_app.urls')),
+    path('todo_app/', include('todo_app.urls')),
     path('admin/', admin.site.urls),
 ]
 
