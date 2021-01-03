68aa5d9ee53226bd6f87b64576eacd92445e766f
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-03 14:43:59 +0300

    - Template and js edited.

--- todo_project/todo_app/static/js/main.js
+++ todo_project/todo_app/static/js/main.js
@@ -37,7 +37,11 @@ window.onload = function () {
 
     $("#add-todo").click(function () {
         var getVal = $("#inputValue").val();
-        console.log(getVal)
+        console.log(getVal);
+        if (getVal === '') {
+            alert("Plase add a new todo!");
+            return
+        }
 
         var xhr = new XMLHttpRequest();
         xhr.open("POST", 'http://localhost:8000/todo_app/todos/', true);
@@ -52,6 +56,73 @@ window.onload = function () {
             console.log(data);
             location.reload();
         };
+    });
+
+    $(".delete-btn").click(function () {
+        todo_id = $(this).attr('about');
+        console.log(todo_id);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("DELETE", 'http://localhost:8000/todo_app/todos/', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            todo_id: todo_id
+        }));
+        xhr.onload = function () {
+            console.log(this.responseText);
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+            location.reload();
+        };
+    });
+
+    $("#inputValue").keypress(function (event) {
+        if (event.keyCode === 13) {
+            $("#add-todo").click();
+        }
+    });
+
+    $(".edited-input").keypress(function (event) {
+        if (event.keyCode === 13) {
+            var v = $(this).parent().parent().children('div:nth-child(4)')
+                .children('div:nth-child(1)')
+                .children('h5:nth-child(1)')
+                .children('i:nth-child(1)');
+            v.click();
+        }
+    });
+
+    $(".edit-btn").click(function () {
+        todo_id = $(this).attr('about');
+
+        var row_input = $(this).parent().eq(3).children('div:nth-child(2)').children('input');
+        row_input.attr("readonly", false);
+        var len = row_input.val().length;
+        row_input[0].focus();
+        row_input[0].setSelectionRange(len, len);
+    });
+
+    $(".save-btn").click(function () {
+        console.log("save");
+        todo_id = $(this).attr('about');
+
+        var row_input = $(this).parent().eq(3).children('div:nth-child(2)').children('input');
+        row_input.attr("readonly", true);
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT", 'http://localhost:8000/todo_app/todos/', true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            todo_id: todo_id,
+            title: row_input.val()
+        }));
+        xhr.onload = function () {
+            console.log(this.responseText);
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+            location.reload();
+        };
 
     });
+
 };
