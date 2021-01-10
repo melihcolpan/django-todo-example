25067c96bf7f7e28eb7431945266b580d0c74120
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-11 02:22:13 +0300

    - App order updated.

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -34,13 +34,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "todo_app.apps.TodoAppConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "todo_app.apps.TodoAppConfig",
 ]
 
 MIDDLEWARE = [
