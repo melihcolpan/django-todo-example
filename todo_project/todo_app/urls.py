0c8097792e2517a61997af1b528cdb91f38a0d60
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-11 06:30:53 +0300

    - Urls updated.

--- todo_project/todo_app/urls.py
+++ todo_project/todo_app/urls.py
@@ -3,6 +3,13 @@
 
 from django.urls import path
 
+from todo_app import views
+
+
 urlpatterns = [
-    # path("", Todos.as_view(), name="todos_one"),
+    path("auth/register", views.register, name="register"),
+    path("auth/login", views.login, name="login"),
+    path("auth/logout", views.logout, name="logout"),
+    path("auth/password_reset", views.password_reset, name="password_reset"),
+    path("auth/password_update", views.password_update, name="password_update"),
 ]
