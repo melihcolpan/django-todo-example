568f273be9a2760e10919d1ef3932d15d9fc5ba5
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-11 06:37:29 +0300

    - Auth validations templates added.

---
+++ todo_project/todo_app/validations.py
@@ -0,0 +1,51 @@
+from cerberus import Validator
+
+
+def validate_register(_in):
+    schema = {
+
+    }
+
+    v = Validator(schema)
+    is_valid = v.validate(_in)
+    return is_valid, v.errors
+
+
+def validate_login(_in):
+    schema = {
+
+    }
+
+    v = Validator(schema)
+    is_valid = v.validate(_in)
+    return is_valid, v.errors
+
+
+def validate_logout(_in):
+    schema = {
+
+    }
+
+    v = Validator(schema)
+    is_valid = v.validate(_in)
+    return is_valid, v.errors
+
+
+def validate_password_reset(_in):
+    schema = {
+
+    }
+
+    v = Validator(schema)
+    is_valid = v.validate(_in)
+    return is_valid, v.errors
+
+
+def validate_password_update(_in):
+    schema = {
+
+    }
+
+    v = Validator(schema)
+    is_valid = v.validate(_in)
+    return is_valid, v.errors
