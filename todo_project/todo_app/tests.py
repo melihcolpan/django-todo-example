e417c956f9aa2c74431f96d1750d18a8572a1af2
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-11 06:30:08 +0300

    - Test templates have been added.

--- todo_project/todo_app/tests.py
+++ todo_project/todo_app/tests.py
@@ -1,2 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+from .models import User, Todo
+
+
+class TodoTestCase(TestCase):
+    def setUp(self):
+
+        self.user = User.objects.create(
+            id=1, is_active=True, email="super_user@example.com", password="secret"
+        )
+        self.todo = Todo.objects.create(id=1, user=self.user)
+        self.access_token = self.user.generate_access_token()
+
+    def test_register(self):
+        request = self.client.get("/register")
+
+    def test_login(self):
+        request = self.client.get("/login")
+
+    def test_logout(self):
+        request = self.client.get("/logout")
+
+    def test_password_reset(self):
+        request = self.client.get("/password_reset")
+
+    def test_password_update(self):
+        request = self.client.get("/password_update")
