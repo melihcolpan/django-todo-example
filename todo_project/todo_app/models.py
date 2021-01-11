ca35b3fc25df220837dac7e5e8d9bbd4f6fa8800
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-11 06:26:28 +0300

    - Token generator added.

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import jwt
+from datetime import datetime, timedelta
+
 from django.db import models
+
+from todo_app.consts import JWT_ALGORITHM, JWT_SECRET, JWT_EXP_DELTA_SECONDS
 
 
 class User(models.Model):
     id = models.AutoField(primary_key=True)
+    is_active = models.BooleanField(default=True)
     username = models.CharField(max_length=48)
+    email = models.CharField(max_length=48, unique=True)
+    password = models.CharField(max_length=48)
     name = models.CharField(max_length=48)
     surname = models.CharField(max_length=48)
-    email = models.CharField(max_length=48)
-    password = models.CharField(max_length=48)
+
+    def generate_access_token(self):
+        payload = {
+            "user_id": self.id,
+            "email": self.email,
+            "is_active": self.is_active,
+            "exp": datetime.utcnow() + timedelta(seconds=JWT_EXP_DELTA_SECONDS),
+        }
+        return jwt.encode(payload, JWT_SECRET, JWT_ALGORITHM)
 
 
 class Todo(models.Model):
@@ -21,5 +36,5 @@ class Todo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     completed_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    expire_at = models.DateTimeField()
+    expire_at = models.DateTimeField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
