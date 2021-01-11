db6827a88ead6296e66a4ae3734e5a4b502fab88
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-11 06:30:30 +0300

    - Views templates have been added.

--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+
+def register(request):
+    pass
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def password_reset(request):
+    pass
+
+def password_update(request):
+    pass
