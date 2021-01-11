88c08b5993c5bb7f36dbbf42a81576000cade1d9
Melih Çolpan <melihcolpan1@gmail.com>
2021-01-11 06:59:41 +0300

    - Register validation completed.

--- todo_project/todo_app/validations.py
+++ todo_project/todo_app/validations.py
@@ -5,8 +5,27 @@ from cerberus import Validator
 
 
 def validate_register(_in):
-    schema = {}
-
+    schema = {
+        "username": {
+            "type": "string",
+            "maxlength": 24,
+            "nullable": False,
+            "requreid": True,
+        },
+        "email": {
+            "type": "string",
+            "maxlength": 48,
+            "nullable": False,
+            "requreid": True,
+            "regex": "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",
+        },
+        "password": {
+            "type": "string",
+            "maxlength": 48,
+            "nullable": False,
+            "requreid": True,
+        },
+    }
     v = Validator(schema)
     is_valid = v.validate(_in)
     return is_valid, v.errors
